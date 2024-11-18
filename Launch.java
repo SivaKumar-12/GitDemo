e21f7f3f4d8cd43a528321f7b866f9539a957478
SivaKumar-12 <ragitisivakumar12@gmail.com>
2024-11-18 10:52:13 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -3,5 +3,12 @@ class Launch
       public static void main(String[] args)
            {
                 System.out.println("Exploring Git & GitHub!");
-           }     
+           }  
+      public static void fun()
+           {
+               int a = 10;
+               int b = 20;
+               int sum = a+b;
+               System.out.println(sum);
+           }   
  }
